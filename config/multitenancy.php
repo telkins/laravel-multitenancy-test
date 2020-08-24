8cf938d60f30e67c51168d8d474b52a6129756cb
Travis Elkins <travis.elkins@gmail.com>
2020-08-24 17:53:28 +0200

    Multitenancy config tenant and landlord connections are specified.

--- config/multitenancy.php
+++ config/multitenancy.php
@@ -51,12 +51,12 @@ return [
      *
      * Set to `null` to use the default connection.
      */
-    'tenant_database_connection_name' => null,
+    'tenant_database_connection_name' => 'tenant',
 
     /*
      * The connection name to reach the landlord database
      */
-    'landlord_database_connection_name' => null,
+    'landlord_database_connection_name' => 'landlord',
 
     /*
      * This key will be used to bind the current tenant in the container.
