1d0389af64e5d2e360ac828edea997bb4746d4e1
Travis Elkins <travis.elkins@gmail.com>
2020-08-25 11:50:25 +0200

    Reordered landlord/tenant configs.

--- config/database.php
+++ config/database.php
@@ -35,12 +35,12 @@ return [
 
     'connections' => [
 
-        'tenant' => [
+        'landlord' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => null,
+            'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
@@ -55,12 +55,12 @@ return [
             ]) : [],
         ],
 
-        'landlord' => [
+        'tenant' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => null,
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
